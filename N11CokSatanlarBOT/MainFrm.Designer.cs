78422b9dd110559aa8bfab7307b978022b0bf20c
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2019-07-30 04:05:48 +0300

    Add files via upload

---
+++ N11CokSatanlarBOT/MainFrm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace N11CokSatanlar
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrunler = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUrunler
+            // 
+            this.txtUrunler.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUrunler.Location = new System.Drawing.Point(12, 12);
+            this.txtUrunler.Multiline = true;
+            this.txtUrunler.Name = "txtUrunler";
+            this.txtUrunler.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtUrunler.Size = new System.Drawing.Size(587, 177);
+            this.txtUrunler.TabIndex = 0;
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 198);
+            this.Controls.Add(this.txtUrunler);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "N11 Çok Satanlar BOT | github.com/dursunkatar";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFrm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrunler;
+    }
+}
+
---
+++ N11CokSatanlarBOT/MainFrm.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N11CokSatanlar
+{
+    public partial class MainFrm : Form
+    {
+        ChromeDriver driver;
+        public MainFrm()
+        {
+            InitializeComponent();
+
+            CheckForIllegalCrossThreadCalls = false;
+            var service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            driver = new ChromeDriver(service);
+            driver.Navigate().GoToUrl("https://www.n11.com/cok-satanlar");
+            var urunAdlari = driver.FindElementsByCssSelector("h3.productName"); ;
+            var urunFiyatlari = driver.FindElementsByCssSelector("div.proDetail ins");
+
+            for (int i = 0; i < urunAdlari.Count; i++)
+            {
+                txtUrunler.AppendText(urunAdlari[i].Text + " ( " + urunFiyatlari[i].Text + " )" + Environment.NewLine + Environment.NewLine);
+            }
+        }
+
+        private void MainFrm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            driver.Quit();
+        }
+    }
+}
