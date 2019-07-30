e798d718ee15bdc10736baf476ff41697952fc67
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2019-07-30 03:57:26 +0300

    Add files via upload

---
+++ MultiThreadWorks/ConnectActions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreadWorks
+{
+    public class Connect
+    {
+        public delegate void CallBack(string result);
+
+        public string AppName { get; set; }
+        public int Second { get; set; }
+
+        public void Start(CallBack func)
+        {
+            Thread.Sleep(500 * Second);
+            func(AppName);
+        }
+    }
+
+    public class ConnectManager
+    {
+        private static volatile object _obj = new object();
+        private static List<string> _liste;
+        public int Count { get; set; }
+
+        static ConnectManager()
+        {
+            _liste = new List<string>();
+        }
+
+        private void callBack(string result)
+        {
+            lock (_obj)
+            {
+                _liste.Add(result);
+            }
+        }
+
+        private bool isFinish()
+        {
+            lock (_obj)
+            {
+                return _liste.Count == Count;
+            }
+        }
+
+        private void startConnect(int i)
+        {
+            Task.Run(() =>
+            {
+                var _con = new Connect
+                {
+                    AppName = "app-" + i + " connected!",
+                    Second = i
+                };
+                _con.Start(callBack);
+            });
+        }
+        public IEnumerable<string> GetList()
+        {
+            for (int i = 1; i <= Count; i++)
+            {
+                startConnect(i);
+            }
+            while (!isFinish()) ;
+            return _liste;
+        }
+    }
+
+}
---
+++ MultiThreadWorks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultiThreadWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectManager = new ConnectManager();
+            connectManager.Count = 5;
+
+            Console.WriteLine("Waiting for list..\n");
+            var list = connectManager.GetList();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nFinish");
+            Console.Read();
+        }
+    }
+}
