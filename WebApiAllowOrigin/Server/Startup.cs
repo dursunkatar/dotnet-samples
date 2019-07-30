311c7b7fe36c778c4b6d8f657e7495ba7eada39b
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2019-07-30 18:46:32 +0300

    Update Startup.cs

--- WebApiAllowOrigin/Server/Startup.cs
+++ WebApiAllowOrigin/Server/Startup.cs
@@ -26,7 +26,6 @@ namespace Server
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
