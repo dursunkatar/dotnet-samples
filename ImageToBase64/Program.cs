ba531ff8496e93bd7daec30a00506811fdc22084
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2019-08-14 15:41:55 +0300

    Add files via upload

---
+++ ImageToBase64/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ImageToBase64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] imageArray = File.ReadAllBytes("C:\image.jpg");
+            string base64ImageRepresentation = Convert.ToBase64String(imageArray);
+            Console.WriteLine(base64ImageRepresentation);
+            Console.ReadKey(true);
+        }
+    }
+}
