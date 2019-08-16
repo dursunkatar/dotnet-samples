727782339005f7d51f8ae20ef2f0897b723aaabd
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2019-08-16 16:16:39 +0300

    Add files via upload

---
+++ AspNetCoreViewComponentUsage/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreViewComponentUsage.Models;
+
+namespace AspNetCoreViewComponentUsage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    
+    }
+}
---
+++ AspNetCoreViewComponentUsage/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCoreViewComponentUsage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreViewComponentUsage.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var menuList = new Menu[] {
+                new Menu { Name="Menü 1"},
+                new Menu { Name="Menü 2"},
+                new Menu { Name="Menü 3"}
+            };
+            return View(menuList);
+        }
+    }
+}
