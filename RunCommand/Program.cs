99f178ae20bc475dbd54b546633c7250a2f3ecc3
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2019-09-24 23:09:42 +0300

    Add files via upload

---
+++ RunCommand/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process pro = new Process();
+            pro.StartInfo.FileName = "cmd.exe";
+            pro.StartInfo.UseShellExecute = false;
+            pro.StartInfo.RedirectStandardInput = true;
+            pro.StartInfo.RedirectStandardOutput = true;
+            pro.StartInfo.RedirectStandardError = true;
+            pro.StartInfo.CreateNoWindow = true;
+            pro.Start();
+            pro.StandardInput.WriteLine("netstat -ano");
+            pro.StandardInput.WriteLine("exit");
+
+            string line = null;
+
+            while ((line = pro.StandardOutput.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+            pro.Close();
+            Console.Read();
+        }
+    }
+}
