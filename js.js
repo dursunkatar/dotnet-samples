ee031fdf3de503d555dd21bf4f81983780600d76
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2022-08-11 12:15:47 +0300

    Update js.js

--- js.js
+++ js.js
@@ -21,7 +21,7 @@
                 method: 'post',
                 body: 'ad=deneme&soyad=falan', // veya new FormData(document.getElementById('form')),
                 headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                 },
             }).then(response => response.json())
               .then(data => console.log(data));
